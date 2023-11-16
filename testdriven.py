06bfdf4aca9fdfde5bac006166f81cb537771a9b
mohan <rajkmohan@gmail.com>
2023-11-16 12:27:59 +0530

    testdriven file is modifid

--- testdriven.py
+++ testdriven.py
@@ -1,1 +1,15 @@
-print("hello python")
+class test:
+    def __init__(self,name,age,city) -> None:
+        self.name=name
+        self.age=age
+        self.city=city
+    def display(self):
+        print(self.name,self.age,self.city)
+        
+class test1(test):
+    def __init__(self, name, age, city) -> None:
+        super().__init__(name, age, city)      # super() is used to call parent class constructor.
+        
+    
+t1=test1("raj",24,"chennai")   #object created by using child class
+t1.display()    # method in parent classs
